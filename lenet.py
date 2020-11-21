270639c20831bc6a04ee4075a7577203adc4b578
dmizr <david.mizrahi@epfl.ch>
2020-11-21 20:36:34 +0100

    follow pep guidelines

--- lenet.py
+++ lenet.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class LeNet5(nn.Module):
     """LeNet-5 model from `"Gradient-Based Learning Applied To Document Recognition"
     <http://yann.lecun.com/exdb/publis/pdf/lecun-98.pdf>`_
