fb5bf126adbdf875620c5d234dea8b59e53531e2
dmizr <david.mizrahi@epfl.ch>
2020-11-21 18:44:20 +0100

    add lenet model

---
+++ lenet.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+
+class LeNet5(nn.Module):
+    """LeNet-5 model from `"Gradient-Based Learning Applied To Document Recognition"
+    <http://yann.lecun.com/exdb/publis/pdf/lecun-98.pdf>`_
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        # Add padding to transform 28x28 MNIST image into 32x32 image
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5, padding=2)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=2)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.maxpool(self.relu(self.conv1(x)))
+        x = self.maxpool(self.relu(self.conv2(x)))
+        x = x.flatten()
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
