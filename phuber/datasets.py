55a1da07e703e44fe4448a7ebaac52387d36b3f0
dmizr <david.mizrahi@epfl.ch>
2020-11-23 02:37:04 +0100

    blacken code

--- phuber/datasets.py
+++ phuber/datasets.py
@@ -45,7 +45,9 @@ class NoisyMNIST(MNIST):
 
             p = np.ones((len(self.targets), self.num_classes))
             p = p * (self.corrupt_prob / (self.num_classes - 1))
-            p[np.arange(len(self.targets)), self.targets.numpy()] = 1 - self.corrupt_prob
+            p[np.arange(len(self.targets)), self.targets.numpy()] = (
+                1 - self.corrupt_prob
+            )
 
             for i in range(len(self.targets)):
                 self.targets[i] = np.random.choice(self.num_classes, p=p[i])
@@ -91,7 +93,9 @@ class NoisyCIFAR10(CIFAR10):
 
             p = np.ones((len(self.targets), self.num_classes))
             p = p * (self.corrupt_prob / (self.num_classes - 1))
-            p[np.arange(len(self.targets)), self.targets.numpy()] = 1 - self.corrupt_prob
+            p[np.arange(len(self.targets)), self.targets.numpy()] = (
+                1 - self.corrupt_prob
+            )
 
             for i in range(len(self.targets)):
                 self.targets[i] = np.random.choice(self.num_classes, p=p[i])
@@ -137,7 +141,9 @@ class NoisyCIFAR100(CIFAR100):
 
             p = np.ones((len(self.targets), self.num_classes))
             p = p * (self.corrupt_prob / (self.num_classes - 1))
-            p[np.arange(len(self.targets)), self.targets.numpy()] = 1 - self.corrupt_prob
+            p[np.arange(len(self.targets)), self.targets.numpy()] = (
+                1 - self.corrupt_prob
+            )
 
             for i in range(len(self.targets)):
                 self.targets[i] = np.random.choice(self.num_classes, p=p[i])
