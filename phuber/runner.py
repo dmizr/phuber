9151966e849abacfd8e2cf1eea4017fb7b971da4
dmizr <david.mizrahi@epfl.ch>
2020-11-30 18:41:24 +0100

    move split_dataset to dataset.py

--- phuber/runner.py
+++ phuber/runner.py
@@ -1,4 +1,3 @@
-import collections
 import logging
 import os
 from typing import Optional, Tuple
@@ -7,10 +6,10 @@ import hydra
 import torch
 from hydra.utils import instantiate
 from omegaconf import DictConfig, OmegaConf
-from torch.utils.data import DataLoader, Dataset, Subset, random_split
+from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
-from phuber.dataset import NoisyCIFAR10, NoisyCIFAR100, NoisyMNIST
+from phuber.dataset import NoisyCIFAR10, NoisyCIFAR100, NoisyMNIST, split_dataset
 from phuber.evaluator import Evaluator
 from phuber.scheduler import ExponentialDecayLR
 from phuber.trainer import Trainer
@@ -285,33 +284,3 @@ def get_loaders(
         test_loader = None
 
     return train_loader, val_loader, test_loader
-
-
-def split_dataset(dataset: Dataset, split: float, seed: int) -> Tuple[Subset, Subset]:
-    """Splits dataset into a train / val set based on a split value and seed
-
-    Args:
-        dataset: dataset to split
-        split: The proportion of the dataset to include in the validation split,
-            must be between 0 and 1.
-        seed: Seed used to generate the split
-
-    Returns:
-        Subsets of the input dataset
-
-    """
-    # Verify that the dataset is Sized
-    if not isinstance(dataset, collections.abc.Sized):
-        raise ValueError("Dataset is not Sized!")
-
-    if not (0 <= split <= 1):
-        raise ValueError(f"Split value must be between 0 and 1. Value: {split}")
-
-    val_length = int(len(dataset) * split)
-    train_length = len(dataset) - val_length
-    splits = random_split(
-        dataset,
-        [train_length, val_length],
-        generator=torch.Generator().manual_seed(seed),
-    )
-    return splits
