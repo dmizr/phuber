d2419219defb3d61ccc68094b915ea022d9220f1
David Ubuntu Desktop <david.mizrahi@epfl.ch>
2020-12-01 19:12:32 +0100

    use logsoftmax for stability

--- phuber/loss.py
+++ phuber/loss.py
@@ -7,12 +7,13 @@ import torch.nn as nn
 class CrossEntropy(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.softmax = nn.Softmax(dim=1)
+        # Use log softmax as it has better numerical properties (avoids NaN)
+        self.log_softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-        p = self.softmax(input)
+        p = self.log_softmax(input)
         p = p[torch.arange(p.shape[0]), target]
-        loss = -torch.log(p)
+        loss = -p
         return torch.mean(loss)
 
 
@@ -20,15 +21,19 @@ class PHuberCrossEntropy(nn.Module):
     def __init__(self, tau: float = 10) -> None:
         super().__init__()
         self.tau = tau
-        self.softmax = nn.Softmax(dim=1)
+        # Use log softmax as it has better numerical properties (avoids NaN)
+        self.log_softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-        p = self.softmax(input)
+
+        p = self.log_softmax(input)
         p = p[torch.arange(p.shape[0]), target]
 
+        # Adapt formula as p is in log-scale
         loss = torch.where(
-            p <= 1 / self.tau,
-            -self.tau * p + math.log(self.tau) + 1,
-            -torch.log(p),
+            p <= math.log(1 / self.tau),
+            -self.tau * torch.exp(p) + math.log(self.tau) + 1,
+            -p,
         )
+
         return torch.mean(loss)
