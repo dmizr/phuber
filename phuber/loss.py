9a2407130c280c45ef6fe2c7d018b6c927e2858a
David Ubuntu Desktop <david.mizrahi@epfl.ch>
2020-12-02 20:17:30 +0100

    add basic documentation to losses

--- phuber/loss.py
+++ phuber/loss.py
@@ -5,6 +5,16 @@ import torch.nn as nn
 
 
 class CrossEntropy(nn.Module):
+    """Computes the cross-entropy loss
+
+    Shape:
+        - Input: the raw, unnormalized score for each class.
+                tensor of size :math:`(minibatch, C)`, with C the number of classes
+        - Target: the labels, tensor of size :math:`(minibatch)`, where each value
+                is :math:`0 \leq targets[i] \leq C-1`
+        - Output: scalar
+    """
+
     def __init__(self) -> None:
         super().__init__()
         # Use log softmax as it has better numerical properties
@@ -13,33 +23,29 @@ class CrossEntropy(nn.Module):
     def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         p = self.log_softmax(input)
         p = p[torch.arange(p.shape[0]), target]
+
         loss = -p
-        return torch.mean(loss)
-
-
-class PHuberCrossEntropyWithLogSoftmax(nn.Module):
-    def __init__(self, tau: float = 10) -> None:
-        super().__init__()
-        self.tau = tau
-        # Use log softmax as it has better numerical properties (avoids NaN)
-        self.log_softmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-
-        p = self.log_softmax(input)
-        p = p[torch.arange(p.shape[0]), target]
-
-        # Adapt formula as p is in log-scale
-        loss = torch.where(
-            p <= math.log(1 / self.tau),
-            -self.tau * torch.exp(p) + math.log(self.tau) + 1,
-            -p,
-        )
 
         return torch.mean(loss)
 
 
 class PHuberCrossEntropy(nn.Module):
+    """Computes the partially Huberised (PHuber) cross-entropy loss, from
+    `"Can gradient clipping mitigate label noise?"
+    <https://openreview.net/pdf?id=rklB76EKPr>`_
+
+    Args:
+        tau: clipping threshold, must be > 1
+
+
+    Shape:
+        - Input: the raw, unnormalized score for each class.
+                tensor of size :math:`(minibatch, C)`, with C the number of classes
+        - Target: the labels, tensor of size :math:`(minibatch)`, where each value
+                is :math:`0 \leq targets[i] \leq C-1`
+        - Output: scalar
+    """
+
     def __init__(self, tau: float = 10) -> None:
         super().__init__()
         self.tau = tau
@@ -51,7 +57,7 @@ class PHuberCrossEntropy(nn.Module):
         p = p[torch.arange(p.shape[0]), target]
 
         loss = torch.empty_like(p)
-        clip = (p <= 1 / self.tau)
+        clip = p <= 1 / self.tau
         loss[clip] = -self.tau * p[clip] + math.log(self.tau) + 1
         loss[~clip] = -torch.log(p[~clip])
 
