c7d8a68da606f59353a921ae13d047c8b199ceb1
Oğuz Kaan Yüksel <okyksl@gmail.com>
2020-12-03 11:56:32 +0300

    add generalized cross-entropy loss

--- phuber/loss.py
+++ phuber/loss.py
@@ -29,6 +29,37 @@ class CrossEntropy(nn.Module):
         return torch.mean(loss)
 
 
+class GeneralizedCrossEntropy(nn.Module):
+    """Computes the generalized cross-entropy loss, from `
+    "Generalized Cross Entropy Loss for Training Deep Neural Networks with Noisy Labels"
+    <https://arxiv.org/abs/1805.07836>`_
+
+    Args:
+        q: Box-Cox transformation parameter, :math:`\in (0,1]`
+
+
+    Shape:
+        - Input: the raw, unnormalized score for each class.
+                tensor of size :math:`(minibatch, C)`, with C the number of classes
+        - Target: the labels, tensor of size :math:`(minibatch)`, where each value
+                is :math:`0 \leq targets[i] \leq C-1`
+        - Output: scalar
+    """
+
+    def __init__(self, q: float = 0.7) -> None:
+        super().__init__()
+        self.q = q
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        p = self.softmax(input)
+        p = p[torch.arange(p.shape[0]), target]
+
+        loss = (1 - p ** self.q) / self.q
+
+        return torch.mean(loss)
+
+
 class UnhingedLoss(nn.Module):
     """Computes the Unhinged (linear) loss, from
     `"Learning with Symmetric Label Noise: The Importance of Being Unhinged"
